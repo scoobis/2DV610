dcbce1b295f4d7afe2ec75d113dfa5104918dbe1
scoobis <gabriel.bjorlin@gmail.com>
2019-11-22 23:54:26 +0100

    REFACTOR

--- src/MathGame/MathQuestions.java
+++ src/MathGame/MathQuestions.java
@@ -2,12 +2,7 @@ package MathGame;
 
 public class MathQuestions {
 	
-	public int simpleAddition(int num1, int num2) {
-		return num1 + num2;
-	}
+	public int simpleAddition(int num1, int num2) { return num1 + num2; }
 	
-	public int simpleSubtraction(int num1, int num2) {
-		return 5;
-		
-	}
+	public int simpleSubtraction(int num1, int num2) { return num1 - num2; }
 }
