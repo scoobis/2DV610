3bca788061966f2773b9e12958669f8d6056b804
scoobis <gabriel.bjorlin@gmail.com>
2019-11-23 00:19:51 +0100

    REFACTOR

--- src/MathGame/MathQuestions.java
+++ src/MathGame/MathQuestions.java
@@ -12,5 +12,5 @@ public class MathQuestions {
 	
 	public int squareRoot (int num) { return (int) Math.sqrt(9); }
 	
-	public int MediumAddition(int num1, int num2, int num3, int num4) { return 10; }
+	public int MediumAddition(int num1, int num2, int num3, int num4) { return num1 + num2 + num3 + num4; }
 }
