2e0a2636d66dc94c7e372ade60f57e45db4da623
scoobis <gabriel.bjorlin@gmail.com>
2019-11-25 22:34:55 +0100

    GREEN

--- src/MathGame/MathGame.java
+++ src/MathGame/MathGame.java
@@ -101,6 +101,8 @@ public class MathGame {
 	public boolean mediumMultiplication(int num1, int num2, int num3) {
 		int answer = question.mediumMultiplication(num1, num2, num3);
 		
+		System.out.print("5 * 5 * 5 = ");
+		
 		return validation(getInput(), answer);
 	}
 	
