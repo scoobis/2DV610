37c90275a1e9340cc3d374596399d831a56a24f8
scoobis <gabriel.bjorlin@gmail.com>
2019-11-25 01:04:17 +0100

    GREEN

--- src/MathGame/MathGame.java
+++ src/MathGame/MathGame.java
@@ -55,6 +55,7 @@ public class MathGame {
 	
 	public boolean simpleMultiplication(int num1, int num2) {
 		int answer =  question.simpleMultiplication(num1, num2);
+		System.out.print("5 * 5 = ");
 		String guess = getInput();
 		return answer == Integer.parseInt(guess);
 	}
